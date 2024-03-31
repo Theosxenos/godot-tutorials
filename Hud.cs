2b62aa951f2d62cd7553ff19de61998f63e16973
Theosxenos <64856229+Theosxenos@users.noreply.github.com>
2024-03-31 20:34:45 +0200

    Finish HUD

---
+++ Hud.cs
@@ -0,0 +1,58 @@
+using Godot;
+using System;
+
+public partial class Hud : CanvasLayer
+{
+	[Signal]
+	public delegate void StartGameEventHandler();
+	
+	[Export] public Label ScoreLabel { get; set; }
+	
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		ScoreLabel.Text = "0";
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+
+	public void ShowMessage(string message)
+	{
+		var label = GetNode<Label>("Message");
+		label.Text = message;
+		label.Show();
+	}
+
+	async public void GameOver()
+	{
+		ShowMessage("Game Over!");
+		GetNode<Timer>("MessageTimer").Start();
+
+		var messageTimer = GetNode<Timer>("MessageTimer");
+		await ToSignal(messageTimer, Timer.SignalName.Timeout);
+		
+		ShowMessage("Dodge the Creeps!");
+
+		await ToSignal(GetTree().CreateTimer(1f), Timer.SignalName.Timeout);
+		GetNode<Button>("StartButton").Show();
+	}
+
+	public void UpdateScore(int score)
+	{
+		ScoreLabel.Text = score.ToString();
+	}
+
+	public void OnMessageTimerTimeOut()
+	{
+		GetNode<Label>("Message").Hide();
+	}
+	
+	private void OnStartButtonPressed()
+	{
+		GetNode<Button>("StartButton").Hide();
+		EmitSignal(SignalName.StartGame);
+	}
+}
