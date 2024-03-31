d2bcc16b73873e3f570b8a4cd6e220965bc95b9e
Theosxenos <64856229+Theosxenos@users.noreply.github.com>
2024-03-31 21:22:39 +0200

    Clear mobs on game start

--- Main.cs
+++ Main.cs
@@ -18,6 +18,7 @@ public partial class Main : Node
 	public void NewGame()
 	{
 		score = 0;
+		GetTree().CallGroup("mobs", Node.MethodName.QueueFree);
 
 		var player = GetNode<Player>("Player");
 		var startPosition = GetNode<Marker2D>("StartPosition");
