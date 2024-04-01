a1c79f34f9cfa15eb5a4ef9bad22fdec75faf0b0
Theosxenos <64856229+Theosxenos@users.noreply.github.com>
2024-04-01 13:09:48 +0200

    Implement mob spawning

---
+++ SquashTheCreeps/Main.cs
@@ -0,0 +1,25 @@
+using Godot;
+using System;
+
+public partial class Main : Node
+{
+	[Export]
+	public PackedScene MobScene { get; set; }
+	
+	private void OnMobTimerTimeout()
+	{
+		var mob = MobScene.Instantiate<Mob>();
+		
+		// Choose a random location on the SpawnPath.
+		// We store the reference to the SpawnLocation node.
+		var mobSpawnLocation = GetNode<PathFollow3D>("SpawnPath/Spawnlocations");
+		// And give it a random offset.
+		mobSpawnLocation.ProgressRatio = GD.Randf();
+
+		Vector3 playerPosition = GetNode<Player>("Player").Position;
+		mob.Initialize(mobSpawnLocation.Position, playerPosition);
+
+		// Spawn the mob by adding it to the Main scene.
+		AddChild(mob);
+	}
+}
