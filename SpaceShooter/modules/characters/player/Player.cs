e66125672f1afed2bc3d6a27a9131b5e31ae81ac
Theosxenos <64856229+Theosxenos@users.noreply.github.com>
2024-04-05 16:51:38 +0200

    Implement player weapon firing

--- SpaceShooter/modules/characters/player/Player.cs
+++ SpaceShooter/modules/characters/player/Player.cs
@@ -1,28 +1,36 @@
 using System;
 using Godot;
-using SpaceShooter.modules.shared.interfaces;
 
-public partial class Player : Area2D, IHitable
+public partial class Player : Area2D
 {
 	[Signal]
 	public delegate void HitEventHandler();
 	
+	[Export] public int Lives { get; set; } = 3;
 	[Export] public int Speed { get; set; } = 400;
-	[Export] public PackedScene Projectile { get; set; }
+	[Export] public PackedScene ProjectileScene { get; set; }
 	
 	public override void _Process(double delta)
 	{
 		var direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
 		Position += direction * Speed * (float)delta;
+
+		if (Input.IsActionJustPressed("fire_weapon"))
+		{
+			var projectile = ProjectileScene.Instantiate<PlayerProjectile>();
+			projectile.Position = GetNode<Marker2D>("Weapon").Position;
+			
+			AddChild(projectile);
+		}
 	}
 
 	private void OnAreaEntered(Area2D area)
 	{
-		GD.Print("Area entered");
-	}
+		if (--Lives <= 0)
+		{
+			QueueFree();
+		}
 
-	void IHitable.Hit()
-	{
-		throw new NotImplementedException();
+		EmitSignal(nameof(HitEventHandler));
 	}
 }
--- SpaceShooter/modules/projectiles/player/PlayerProjectile.cs
+++ SpaceShooter/modules/projectiles/player/PlayerProjectile.cs
@@ -1,22 +1,22 @@
 using Godot;
-using System;
-using SpaceShooter.modules.shared.interfaces;
 
 public partial class PlayerProjectile : Area2D
 {
-    [Export] public int Speed { get; set; } = 800;
-    public override void _Process(double delta)
-    {
-        Position += Vector2.Right * Speed * (float)delta;
-    }
+	[Export] public int Speed { get; set; } = 800;
+	public override void _Process(double delta)
+	{
+		Position += Vector2.Right * Speed * (float)delta;
+	}
 
-    public void OnAreaEntered(Area2D area)
-    {
-        if (area is IHitable)
-        {
-            ((IHitable)area).Hit();
-        }
-        
-        QueueFree();
-    }
+	public void OnAreaEntered(Area2D area)
+	{
+		QueueFree();
+	}
+
+	private void OnVisibleOnScreenNotifier2dScreenExited()
+	{
+		QueueFree();
+	}
 }
+
+
