1cc65393b8b5bb5890f4871b738c5e1d9913d56a
Theosxenos <64856229+Theosxenos@users.noreply.github.com>
2024-04-08 16:57:12 +0200

    Finish up to part 3

---
+++ Heart Action RPG/modules/characters/player/player.gd
@@ -0,0 +1,23 @@
+extends CharacterBody2D
+
+@export var acceleration = 300
+@export var max_speed = 120
+@export var friction = 400
+
+var motion : Vector2
+
+func _ready():
+	pass
+
+func _physics_process(delta):
+	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	
+	if direction.length() > 0:
+		#velocity += direction * acceleration * delta
+		#velocity = velocity.limit_length(max_speed)
+		velocity = velocity.move_toward(direction * max_speed, acceleration * delta)
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, friction * delta)
+	
+	#move_and_collide(velocity * delta)
+	move_and_slide()
