f275952206f205009525c63b50deb835948caeac
Theosxenos <64856229+Theosxenos@users.noreply.github.com>
2024-04-08 19:18:09 +0200

    Finish part 6

--- Heart Action RPG/modules/characters/player/player.gd
+++ Heart Action RPG/modules/characters/player/player.gd
@@ -5,6 +5,8 @@ extends CharacterBody2D
 @export var friction = 400
 
 @onready var animation_player = $AnimationPlayer
+@onready var animation_tree: AnimationTree = $AnimationTree as AnimationTree
+@onready var animation_state = animation_tree.get("parameters/playback")
 
 func _ready():
 	pass
@@ -13,16 +15,16 @@ func _physics_process(delta):
 	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	
 	if direction.length() > 0:
-		if direction.x > 0:
-			animation_player.play("run_right")
-		else:
-			animation_player.play("run_left")
-		
+		animation_tree.set("parameters/Idle/blend_position", direction)
+		animation_tree.set("parameters/Run/blend_position", direction)
+		animation_state.travel("Run")
+
 		#velocity += direction * acceleration * delta
 		#velocity = velocity.limit_length(max_speed)
 		velocity = velocity.move_toward(direction * max_speed, acceleration * delta)
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, friction * delta)
-	
+		animation_state.travel("Idle")
+
 	#move_and_collide(velocity * delta)
 	move_and_slide()
