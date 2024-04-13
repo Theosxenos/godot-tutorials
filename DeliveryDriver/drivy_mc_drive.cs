4029df6c0a24aa42c0d23c8e124ec65954245cc3
Theosxenos <64856229+Theosxenos@users.noreply.github.com>
2024-04-13 13:50:34 +0200

    Add assets, basic level

--- DeliveryDriver/drivy_mc_drive.cs
+++ DeliveryDriver/drivy_mc_drive.cs
@@ -13,11 +13,17 @@ public partial class drivy_mc_drive : CharacterBody2D
 		var steerAmount = Input.GetAxis("ui_left","ui_right");
 		var moveDirection = Input.GetAxis("ui_up", "ui_down");
 
+		// if (steerAmount != 0 && moveDirection == 0)
+		// {
+		// 	moveDirection = -1;
+		// }
+
 		RotationDegrees += steerAmount * SteerSpeed * (float)delta;
 
 		var direction = new Vector2(0f, moveDirection * MoveSpeed * (float)delta).Rotated(Rotation);
 		// Velocity = Transform.X * moveDirection * MoveSpeed * (float)delta;
-		Position += direction;
+		// Position += direction;
+		Velocity = Transform.Y * MoveSpeed * moveDirection;
 		MoveAndSlide();
 	}
 }
