ff693f5a64b63fda570ccd2c41b9b3fd470d25f0
Theosxenos <64856229+Theosxenos@users.noreply.github.com>
2024-05-22 13:36:23 +0200

    Up to 2.11

--- dungeon-rpg/Scenes/Characters/Player/Player.cs
+++ dungeon-rpg/Scenes/Characters/Player/Player.cs
@@ -13,19 +13,25 @@ public partial class Player : CharacterBody3D
 
     public override void _PhysicsProcess(double delta)
     {
-        AnimationPlayer.Play(direction != Vector2.Zero ? GameConstants.ANIM_MOVE : GameConstants.ANIM_IDLE);
-
-        Sprite.FlipH = direction.X < 0;
-        
         Velocity = new(direction.X, 0, direction.Y);
         Velocity *= Speed;
 
         MoveAndSlide();
+
+        FlipSprite();
+    }
+
+    private void FlipSprite()
+    {
+        // Not moving horizontally
+        if(Velocity.X == 0) return;
+
+        Sprite.FlipH = direction.X < 0;
     }
 
     public override void _Input(InputEvent @event)
     {
         direction = Input.GetVector(GameConstants.INPUT_MOVE_LEFT, GameConstants.INPUT_MOVE_RIGHT, GameConstants.INPUT_MOVE_FORWARD, GameConstants.INPUT_MOVE_BACKWARD);
-
+        AnimationPlayer.Play(direction != Vector2.Zero ? GameConstants.ANIM_MOVE : GameConstants.ANIM_IDLE);
     }
 }
