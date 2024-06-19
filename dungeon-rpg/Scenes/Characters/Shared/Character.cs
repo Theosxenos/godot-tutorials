5ebba00688c66b13e3923399ed9bdd1777034d18
Theosxenos <64856229+Theosxenos@users.noreply.github.com>
2024-06-19 19:40:58 +0200

    Finish 6.5 Hitboxes and hurtboxes

--- dungeon-rpg/Scenes/Characters/Shared/Character.cs
+++ dungeon-rpg/Scenes/Characters/Shared/Character.cs
@@ -8,6 +8,7 @@ public abstract partial class Character : CharacterBody3D
     [Export] public Sprite3D Sprite { get; private set; }
     [Export] public AnimationPlayer AnimationPlayer { get; private set; }
     [Export] public StateMachine StateMachine { get; private set; }
+    [Export] public Area3D HurtboxNode { get; private set; }
     
     [ExportGroup("AI Nodes")]
     [Export] public Path3D PathNode { get; private set; }
@@ -16,12 +17,28 @@ public abstract partial class Character : CharacterBody3D
     [Export] public Area3D AttackAreaNode { get; private set; }
     
     public Vector2 Direction { get; set; } = Vector2.Zero;
-    
+
+    public override void _Ready()
+    {
+        if (HurtboxNode == null)
+        {
+            GD.PushWarning($"Hitbox not set for {Name}");
+            return;
+        }
+        
+        HurtboxNode.AreaEntered += HurtboxNodeOnAreaEntered;
+    }
+
     public void FlipSprite()
     {
         // Not moving horizontally
         if (Velocity.X == 0) return;
 
         Sprite.FlipH = Direction.X < 0;
+    }
+    
+    protected virtual void HurtboxNodeOnAreaEntered(Area3D area)
+    {
+        GD.Print($"{area.Name} hit");
     }
 }
